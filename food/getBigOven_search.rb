2c4cb8ba60cb08e307dd3b0f4d74cc47ead5fb79
“zhichaoSellics” <zhichao1208@gmail.com>
2017-11-29 17:55:43 +0100

    how to find mains

--- food/getBigOven_search.rb
+++ food/getBigOven_search.rb
@@ -69,9 +69,9 @@ File.open("/Users/li/Downloads/Script/gitFiles/food/glossList.json","w") do |f|
 end
 =end
 
-data = JSON.parse(File.read("/Users/li/Downloads/Script/gitFiles/food/glossList.json"))
+list = JSON.parse(File.read("/Users/li/Downloads/Script/gitFiles/food/glossList.json"))
 
-data = data.map{|item| item["name"]}.flatten!.sort.slice_when{|i,j| j!=i}.map{|item| {"name"=>item[0],"count"=>item.count}}.sort_by{|item| item["count"]}.reverse
+list = list.map{|item| item["name"]}.flatten!.sort.slice_when{|i,j| j!=i}.map{|item| {"name"=>item[0],"count"=>item.count}}.sort_by{|item| item["count"]}.reverse
 
 sides = ["parmesan","parsley","lemon","garlic","onion"]
 
@@ -91,23 +91,52 @@ data << JSON.parse(File.read("/Users/li/Downloads/Script/gitFiles/food/recipeLin
 
 data.flatten!.uniq!
 
-puts data.count
+puts list[0]
 
-data[0..10].each do |item|	
+
+data[0..1].each do |item|	
 
 	page = Nokogiri::HTML(open(item))
 
-	puts title = page.title
+	 title = page.title
 
 	 # puts glossList2 = page.css("span[class = 'name']")
 
-	glossList = page.css("a[class = 'glosslink']").map{|item| item.text.downcase}
+	glossLists = page.css("a[class = 'glosslink']").map{|item| item.text.downcase}
+
+	glossListsWithVote = []
+
+	glossLists.each do |gloss|
+
+		thing = list.find{|item| item["name"] == gloss}
 
 
-	puts mains = glossList&mains
-	puts "-----"
-	puts diff = glossList - mains
-	puts "======"
+		if !thing.nil?
+
+			glossListsWithVote << [gloss,thing["count"]]
+
+		else
+
+			glossListsWithVote << [gloss,0]
+
+		end
+
+
+end
+
+	mains5 = glossListsWithVote.sort_by{|item| item[1]}.reverse[0..5]
+
+	mains3 = glossListsWithVote.sort_by{|item| item[1]}.reverse[0..3]
+
+	others = glossListsWithVote.sort_by{|item| item[1]}.reverse
+
+	puts "mains5 is #{mains5.to_s}"
+	puts "mains3 is #{mains3.to_s}"
+	puts "others is #{others.to_s}"
+	puts "--------"
 
 	sleep 1
+
+
+
 end
